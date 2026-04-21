d92e0116f6c814624a143522304cc1544ee914fd
Alejandro M Suarez <alemsuarez@gmail.com>
2026-04-21 17:21:44 -0300

    se agrega env para nueva carpeta vite

--- vite.config.js
+++ vite.config.js
@@ -23,15 +23,16 @@ export default defineConfig(({ mode }) => {
   };
 });
 */
+
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   const isProduction = env.VITE_PRODUCTION === 'true'
-
+  const subFolder = import.meta.env.VITE_BASE;
   return {
     plugins: [react()],
-    base: isProduction ? '/help2/incidentPortal/' : '/',
+    base: isProduction ? `/help2/${subFolder}/` : '/',
   }
 })
